ccd84402f31bd00984e93edbeaf35d9fd676ca9d
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2024-10-02 14:54:46 -0500

    Update 00_Group2_Ent_Struc.sql
    Added code for creating Groups table and its constraints. Added "GO" command below the code for creating the Agents table.
    
    Changes to the constraints that conflict with the Data Dictionary needed to be made, as the GroupsPage and GroupsEmail fields would not allow more than one row to contain a null value if they were set to Unique.

--- 00_Group2_Ent_Struc.sql
+++ 00_Group2_Ent_Struc.sql
@@ -5,12 +5,12 @@ CREATE DATABASE Entertainment;
 
 USE Entertainment;
 
-
 -- Agents table
 /*
 Notes:
 Added constraint to AgentsKey to force it to start with the letter A
 Updated column names to account for table name change
+Certain constraints may be useful to copy over to and adapt for other tables.
 */
 
 CREATE TABLE Agents
@@ -24,7 +24,7 @@ CREATE TABLE Agents
 	AgentsState char(2) DEFAULT 'WA' NOT NULL,
 	AgentsZip char(5) NOT NULL,
 	AgentsPhone varchar(15) NOT NULL,
-	AgentsDateHired date NOT NULL,
+	AgentsDateHired Date NOT NULL,
 	AgentsSalary decimal(10,2) NOT NULL,
 	AgentsCommissionRate decimal (4,3) NOT NULL,
 
@@ -42,20 +42,58 @@ CREATE TABLE Agents
 	
 	-- Forces AgentsState to be a valid state abbreviation
 	CONSTRAINT Valid_State_Agents CHECK (AgentsState IN ('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-														'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-														'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-														'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-														'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'))
+								'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+								'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+								'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+								'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'))
 );
-
+GO
 
 -- Groups_Performers table
 
 
 
+
+
 -- Groups table
+/*
+Notes:
+Changed GroupsPage and GroupsEmail to not be unique. This is to allow multiple rows to have a value of NULL in those fields.
+*/
 
+CREATE TABLE Groups
+(
+	-- Groups Table Columns
+	GroupsKey varchar(8) NOT NULL,
+	GroupsName varchar(50) NOT NULL,
+	GroupsSSN char(11) UNIQUE NOT NULL,
+	GroupsAddress varchar(50) NOT NULL,
+	GroupsCity varchar(50) NOT NULL,
+	GroupsState char(2) DEFAULT 'WA' NOT NULL,
+	GroupsZip char(5) NOT NULL,
+	GroupsPhone varchar(15) NOT NULL,
+	GroupsPage varchar(50) NULL,
+	GroupsEmail varchar(50) NULL,
+	GroupsDateEntered Date NOT NULL,
 
+	-- Groups Table Constraints
+	CONSTRAINT PK_Groups PRIMARY KEY (GroupsKey),
+	CONSTRAINT Valid_Key_Groups CHECK (GroupsKey LIKE 'G%'), -- Forces GroupsKey to start with A
+	CONSTRAINT Valid_Zip_Groups CHECK (GroupsZip NOT LIKE '%[^0-9]%'), -- Forces GroupsZip column to only consist of digits by using NOT LIKE
+	CONSTRAINT Valid_SSN_Groups CHECK (GroupsSSN LIKE '[0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]'), -- Forces GroupsSSN to follow proper SSN format
+
+	-- Forces GroupsPhone to use proper phone number format, with or without area code
+	CONSTRAINT Valid_Phone_Groups CHECK (GroupsPhone LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'
+						OR GroupsPhone LIKE '([0-9][0-9][0-9])[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
+	
+	-- Forces GroupsState to be a valid state abbreviation
+	CONSTRAINT Valid_State_Groups CHECK (GroupsState IN ('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+								'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
+								'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
+								'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
+								'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY'))
+);
+GO
 
 -- Musical_Styles table
 
