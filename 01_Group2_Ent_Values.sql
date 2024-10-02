f5f81dc44c09b777044b78c20965d8853289156f
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2024-10-02 13:29:25 -0500

    Update 01_Group2_Ent_Values.sql
    Added code to insert values into the Agents table

--- 01_Group2_Ent_Values.sql
+++ 01_Group2_Ent_Values.sql
@@ -4,7 +4,17 @@
 USE Entertainment;
 
 -- Agents table
-
+INSERT INTO Agents (AgentsKey, AgentsFirstName, AgentsLastName, AgentsAddress, AgentsCity, AgentsState, AgentsZip, AgentsPhone, AgentsDateHired, AgentsSalary, AgentsCommissionRate)
+VALUES
+	('A001', 'William', 'Thompson',	'122 Spring River Drive', 'Redmond', 'WA', '98053',	'555-2681',	'05/10/2018', 45000, 0.040),
+	('A002', 'Scott', 'Bishop', '66 Spring Valley Drive', 'Seattle', 'WA', '98125', '555-2666', '01/31/2019', 37000, 0.040),
+	('A003', 'Carol', 'Viescas', '667 Red River Road', 'Bellevue', 'WA', '98006', '555-2571', '11/14/2018', 40000, 0.050),
+	('A004', 'Karen', 'Smith', '30301 - 166th Ave. N.E.', 'Seattle', 'WA', '98125', '555-2551',	'02/28/2019', 32000, 0.055),
+	('A005', 'Marianne', 'Wier', '908 W. Capital Way', 'Tacoma', 'WA', '98413',	'555-2606',	'01/28/2019', 34500, 0.045),
+	('A006', 'John', 'Kennedy',	'16679 NE 41st Court', 'Seattle', 'WA', '98125', '555-2621', '05/10/2018', 43000, 0.060),
+	('A007', 'Caleb', 'Viescas', '4501 Wetland Road', 'Redmond', 'WA', '98052', '555-0037',	'02/11/2019', 32100, 0.035),
+	('A008', 'Maria', 'Patterson', '3445 Cheyenne Road', 'Bellevue', 'WA', '98006', '555-2291',	'08/29/2018',	40000, 0.040),
+	('A009', 'Darryl', 'Mars', '1234 Main Street',	'Kirkland',	'WA', '98033', '555-1234', '01/30/2021', 50, 0.010);
 
 
 -- Groups_Performers table
