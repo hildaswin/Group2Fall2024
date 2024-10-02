a91c1dbb41f21270cffabb5ac210d1301c8cc494
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2024-10-02 15:14:10 -0500

    Update 01_Group2_Ent_Values.sql
    Added "INSERT INTO" command for populating the Groups table.

--- 01_Group2_Ent_Values.sql
+++ 01_Group2_Ent_Values.sql
@@ -4,24 +4,38 @@
 USE Entertainment;
 
 -- Agents table
-INSERT INTO Agents (AgentsKey, AgentsFirstName, AgentsLastName, AgentsAddress, AgentsCity, AgentsState, AgentsZip, AgentsPhone, AgentsDateHired, AgentsSalary, AgentsCommissionRate)
+INSERT INTO Agents
 VALUES
-	('A001', 'William', 'Thompson',	'122 Spring River Drive', 'Redmond', 'WA', '98053',	'555-2681',	'05/10/2018', 45000, 0.040),
-	('A002', 'Scott', 'Bishop', '66 Spring Valley Drive', 'Seattle', 'WA', '98125', '555-2666', '01/31/2019', 37000, 0.040),
-	('A003', 'Carol', 'Viescas', '667 Red River Road', 'Bellevue', 'WA', '98006', '555-2571', '11/14/2018', 40000, 0.050),
-	('A004', 'Karen', 'Smith', '30301 - 166th Ave. N.E.', 'Seattle', 'WA', '98125', '555-2551',	'02/28/2019', 32000, 0.055),
-	('A005', 'Marianne', 'Wier', '908 W. Capital Way', 'Tacoma', 'WA', '98413',	'555-2606',	'01/28/2019', 34500, 0.045),
-	('A006', 'John', 'Kennedy',	'16679 NE 41st Court', 'Seattle', 'WA', '98125', '555-2621', '05/10/2018', 43000, 0.060),
-	('A007', 'Caleb', 'Viescas', '4501 Wetland Road', 'Redmond', 'WA', '98052', '555-0037',	'02/11/2019', 32100, 0.035),
-	('A008', 'Maria', 'Patterson', '3445 Cheyenne Road', 'Bellevue', 'WA', '98006', '555-2291',	'08/29/2018',	40000, 0.040),
-	('A009', 'Darryl', 'Mars', '1234 Main Street',	'Kirkland',	'WA', '98033', '555-1234', '01/30/2021', 50, 0.010);
-
+	('A001', 'William',	'Thompson',	'122 Spring River Drive', 	'Redmond', 	'WA',	'98053', '555-2681',		'05/10/2018', 45000, 	0.040),
+	('A002', 'Scott',	'Bishop', 	'66 Spring Valley Drive', 	'Seattle', 	'WA', 	'98125', '555-2666', 		'01/31/2019', 37000, 	0.040),
+	('A003', 'Carol',	'Viescas', 	'667 Red River Road', 		'Bellevue', 	'WA', 	'98006', '555-2571', 		'11/14/2018', 40000, 	0.050),
+	('A004', 'Karen',	'Smith', 	'30301 - 166th Ave. N.E.', 	'Seattle', 	'WA',	'98125', '555-2551', 		'02/28/2019', 32000, 	0.055),
+	('A005', 'Marianne', 	'Wier', 	'908 W. Capital Way', 		'Tacoma', 	'WA', 	'98413', '555-2606', 		'01/28/2019', 34500, 	0.045),
+	('A006', 'John', 	'Kennedy', 	'16679 NE 41st Court', 		'Seattle', 	'WA', 	'98125', '555-2621', 		'05/10/2018', 43000,	0.060),
+	('A007', 'Caleb', 	'Viescas', 	'4501 Wetland Road', 		'Redmond', 	'WA', 	'98052', '555-0037', 		'02/11/2019', 32100, 	0.035),
+	('A008', 'Maria', 	'Patterson', 	'3445 Cheyenne Road', 		'Bellevue', 	'WA', 	'98006', '555-2291', 		'08/29/2018', 40000, 	0.040),
+	('A009', 'Darryl', 	'Mars', 	'1234 Main Street',		'Kirkland', 	'WA', 	'98033', '555-1234', 		'01/30/2021', 50, 	0.010);
 
 -- Groups_Performers table
 
 
 
 -- Groups table
+INSERT INTO Groups
+VALUES
+	('G001', 'Carol Peacock Trio',		'888-90-1121',	'4110 Old Redmond Rd.',		'Redmond',	'WA',	'98052', '555-2691',		'www.cptrio.com',		'carolp@cptrio.com',		'05/24/1997'),
+	('G002', 'Topazz', 			'888-50-1061',	'16 Maple Lane',		'Auburn',	'WA',	'98002', '555-2591',		'www.topazz.com', 		NULL, 				'02/14/1996'),
+	('G003', 'JV & the Deep Six',		'888-18-1013',	'15127 NE 24th, #383',		'Redmond',	'WA',	'98052', '555-2511',		'www.jvd6.com',			'jv@myspring.com',		'03/18/1998'),
+	('G004', 'Jim Glynn',			'888-26-1025',	'13920 S.E. 40th Street',	'Bellevue',	'WA',	'98009', '555-2531',		NULL, 				NULL, 				'04/01/1996'),
+	('G005', 'Jazz Persuasion',		'888-30-1031',	'233 West Valley Hwy',		'Bellevue',	'WA',	'98005', '555-2541',		'www.jazzper.com',		NULL, 				'05/12/1997'),
+	('G006', 'Modern Dance',		'888-66-1085',	'Route 2, Box 203B',		'Woodinville',	'WA',	'98072', '555-2631',		'www.moderndance.com',		'mikeh@moderndance.com',	'05/16/1995'),
+	('G007', 'Coldwater Cattle Company',	'888-38-1043',	'4726 - 11th Ave. N.E.',	'Seattle',	'WA',	'98105', '555-2561',		'www.coldwatercows.com',	NULL, 				'11/30/1995'),
+	('G008', 'Country Feeling',		'888-98-1133',	'PO Box 223311',		'Seattle',	'WA',	'98125', '555-2711',		NULL,				NULL, 				'02/28/1996'),
+	('G009', 'Katherine Ehrlich',		'888-61-1103',	'777 Fenexet Blvd',		'Woodinville',	'WA',	'98072', '555-0399',		NULL,				'ke@mzo.com',			'09/13/1998'),
+	('G010', 'Saturday Revue',		'888-64-1109',	'3887 Easy Street',		'Seattle',	'WA',	'98125', '555-0039',		'www.satrevue.com',		'edz@coolness.com',		'01/20/1995'),
+	('G011', 'Julia Schnebly',		'888-65-1111',	'2343 Harmony Lane',		'Seattle',	'WA',	'99837', '555-9936',		NULL,	 			NULL,				'04/12/1996'),
+	('G012', 'Susan McLain',		'888-70-1121',	'511 Lenora Ave',		'Bellevue',	'WA',	'98006', '555-2301',		'www.greensleeves.com',		'susan@gs.com',			'10/12/1998'),
+	('G013', 'Caroline Coie Cuartet',	'888-71-1123',	'298 Forest Lane',		'Auburn',	'WA',	'98002', '555-2306',		NULL,				'carolinec@willow.com',		'07/11/1997');
 
 
 
