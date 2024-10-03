10484ec8c3b3642e10463034b4a3391a01e46510
IzzetCoder <136272211+IzzetCoder@users.noreply.github.com>
2024-10-03 14:30:29 -0500

    Added Performers Table

--- 00_Group2_Ent_Struc.sql
+++ 00_Group2_Ent_Struc.sql
@@ -32,7 +32,7 @@ CREATE TABLE [Musical_Styles]
 	-- Primary Key (MusicalStylesKey)
 	CONSTRAINT PK_MusicalStyles PRIMARY KEY (MusicalStylesKey),
 	-- Ensure key starts with 'S'
-	CONSTRAINT Valid_Key_Musical_Styles CHECK (MusicalStylesKey LIKE 'S%'),
+	CONSTRAINT Valid_Key_Musical_Styles CHECK (MusicalStylesKey LIKE 'S%')
 );
 GO
 
@@ -42,7 +42,28 @@ GO
 
 
 -- Performers table
+CREATE TABLE [Performers]
+(
+	-- Performers Table Columns
+	[PerformersKey] varchar(8) NOT NULL,
+	[PerformersFirstName] varchar(50) NOT NULL,
+	[PerformersLastName] varchar(50) NOT NULL,
+	[PerformersSuffix] varchar(10) NULL,
+	[PerformersPhone] varchar(15) NOT NULL,
+	[PerformersGender] char(3) NOT NULL,
 
+	-- Primary Key (PerformersKey)
+	CONSTRAINT PK_Performers PRIMARY KEY (PerformersKey),
+	-- Ensure key starts with 'M'.
+	CONSTRAINT Valid_Key_Performers CHECK (PerformersKey LIKE 'M%'),
+	-- Ensure phone number is of valid format.
+	-- Accepted formats: [###-####], [(###)###-####]
+	CONSTRAINT Valid_Phone_Performers CHECK (PerformersPhone LIKE '[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'
+										OR PerformersPhone LIKE '([0-9][0-9][0-9])[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]'),
+	-- Ensure gender is of valid value.
+	CONSTRAINT Valid_Gender_Performers CHECK (PerformersGender IN ('M', 'F'))
+);
+GO
 
 
 -- Engagements table
