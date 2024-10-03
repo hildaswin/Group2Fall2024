0985c39f670cb708d84f22f933cad33df6761a57
IzzetCoder <136272211+IzzetCoder@users.noreply.github.com>
2024-10-03 15:00:29 -0500

    Inserted values to the Musical_Styles table

--- 01_Group2_Ent_Values.sql
+++ 01_Group2_Ent_Values.sql
@@ -1,7 +1,8 @@
 -- Group 2
 -- Adam Hanson, Alexis Brewers, Colton Bondhus, Ismail Abdullahi, Lily Rossman
 
-USE Entertainment;
+USE [Entertainment]
+GO
 
 -- Agents table
 
@@ -16,7 +17,33 @@ USE Entertainment;
 
 
 -- Musical_Styles table
-
+INSERT INTO [Musical_Styles] ([MusicalStylesKey], [MusicalStylesName])
+VALUES
+	('S001',	'40''s Ballroom Music'),
+	('S002',	'50''s Music'),
+	('S003',	'60''s Music'),
+	('S004',	'70''s Music'),
+	('S005',	'80''s Music'),
+	('S006',	'Country'),
+	('S007',	'Classical'),
+	('S008',	'Classic Rock & Roll'),
+	('S009',	'Rap'),
+	('S010',	'Contemporary'),
+	('S011',	'Country Rock'),
+	('S012',	'Elvis'),
+	('S013',	'Folk'),
+	('S014',	'Chamber Music'),
+	('S015',	'Jazz'),
+	('S016',	'Karaoke'),
+	('S017',	'Motown'),
+	('S018',	'Modern Rock'),
+	('S019',	'Rhythm and Blues'),
+	('S020',	'Show Tunes'),
+	('S021',	'Standards'),
+	('S022',	'Top 40 Hits'),
+	('S023',	'Variety'),
+	('S024',	'Salsa'),
+	('S025',	'90''s Music');
 
 
 -- Customers table
