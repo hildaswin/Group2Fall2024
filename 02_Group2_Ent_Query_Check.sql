0d4a311e7b4f1740c41a2ef51de465537818838d
hildaswin <larossman05@gmail.com>
2024-10-05 16:57:31 -0500

    Added queries 13-15

--- 02_Group2_Ent_Query_Check.sql
+++ 02_Group2_Ent_Query_Check.sql
@@ -27,9 +27,23 @@
 
 --13. Create a report of the number of engagements each group has performed
 
+SELECT G.GroupStageName AS [Stage Name], COUNT(E.EngagementKey) AS [Number Of Engagements]
+FROM Groups G
+LEFT JOIN Engagements E ON G.GroupKey = E.GroupKey
+GROUP BY G.GroupStageName
+ORDER BY [Number Of Engagements] DESC, [Stage Name];
+
 --14. Create a report of the average agent salary
 
+SELECT CAST(AVG(AgentSalary) AS DECIMAL(10, 2)) AS [Average Agent Salary]
+FROM Agents;
+
 --15. Show our earliest October engagement in 2022
+
+SELECT TOP 1 EngagementKey, EngStartDateTime AS [Earliest October Engagement]
+FROM Engagements
+WHERE YEAR(EngStartDateTime) = 2022 AND MONTH(EngStartDateTime) = 10
+ORDER BY EngStartDateTime;
 
 --16. Show the value of our October 2022 bookings
 
